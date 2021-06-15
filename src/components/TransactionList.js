e221743c5bced4b367cc9b897386eae0566fda1d
aditya patel <patel.aditya2014@gmail.com>
2021-06-15 16:39:45 +0530

    transaction list component added

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { FaMoneyBill } from 'react-icons/fa';
+import { FiDelete } from 'react-icons/fi';
 
 export const TransactionList = () => {
   return (
     <div className='transaction-list'>
       <h4>history</h4>
       <ul>
-        <li className='transaction-single'>
-          <p>aditya</p>
+        <li className='displayed-list'>
+          <FaMoneyBill className='bill' />
+          <p className='title'>aditya</p>
+          <FiDelete className='delete-btn' />
         </li>
       </ul>
     </div>
