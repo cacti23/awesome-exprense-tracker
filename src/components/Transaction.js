eabd8762e47c5292ee2896a115fe67e445445501
aditya patel <patel.aditya2014@gmail.com>
2021-06-16 10:17:45 +0530

    delete tansaction functionality implemented

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 import { FaMoneyBill } from 'react-icons/fa';
 import { RiDeleteBack2Fill } from 'react-icons/ri';
 
-export const Transaction = ({ title, amount, isExpense }) => {
+export const Transaction = ({ id, title, amount, isExpense }) => {
+  const { deleteTransaction } = useContext(GlobalContext);
   return (
     <>
       <li className='displayed-list'>
         <FaMoneyBill className={`bill ${isExpense ? 'income' : 'expense'}`} />
         <p className='title'>{title}</p>
         <p className='amount'>${amount}</p>
-        <RiDeleteBack2Fill className='delete-btn' />
+        <RiDeleteBack2Fill
+          className='delete-btn'
+          onClick={() => deleteTransaction(id)}
+        />
       </li>
     </>
   );
--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,7 +1,12 @@
-import React from 'react';
-
 export const AppReducer = (state, action) => {
   switch (action.type) {
+    case 'DELETE_TRANSACTION':
+      return {
+        ...state,
+        transactions: state.transactions.filter(
+          transaction => transaction.id !== action.payload
+        ),
+      };
     default:
       return state;
   }
