61b28523b2ba2e7d9c869bd58a860c82ea20a7e2
aditya patel <patel.aditya2014@gmail.com>
2021-07-07 11:58:41 +0530

    intial router setup done

--- server.js
+++ server.js
@@ -5,9 +5,13 @@ const morgan = require('morgan');
 
 dotenv.config({ path: './config/config.env' });
 
+const transactions = require('./routes/transactions');
+
 const app = express();
 
-app.get('/', (req, res) => res.send('hello'));
+app.use(express.json());
+
+app.use('/api/v1/transactions', transactions);
 
 const PORT = process.env.PORT || 5000;
 
