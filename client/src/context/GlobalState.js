c55a349fc32eda2ff30fad3e6896d46c60e91189
aditya patel <patel.aditya2014@gmail.com>
2021-07-07 14:17:12 +0530

    global state getTransaction implemented

--- client/src/context/GlobalState.js
+++ client/src/context/GlobalState.js
@@ -1,14 +1,34 @@
 import React, { createContext, useReducer } from 'react';
 import { AppReducer } from './AppReducer';
+import axios from 'axios';
 
 const initialState = {
   transactions: [],
+  error: null,
+  loading: true,
 };
 
 export const GlobalContext = createContext(initialState);
 
 export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  // Actions
+  async function getTransactions() {
+    try {
+      const res = await axios.get('/api/v1/transactions');
+
+      dispatch({
+        type: 'GET_TRANSACTIONS',
+        payload: res.data.data,
+      });
+    } catch (error) {
+      dispatch({
+        type: 'TRANSACTION_ERROR',
+        payload: error.response.data.error,
+      });
+    }
+  }
 
   function calculateTotalIncome() {
     return state.transactions
@@ -41,6 +61,7 @@ export const GlobalProvider = ({ children }) => {
     <GlobalContext.Provider
       value={{
         transactions: state.transactions,
+        getTransactions,
         deleteTransaction,
         addTransaction,
         calculateTotalIncome,
